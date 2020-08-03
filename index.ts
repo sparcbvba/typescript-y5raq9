bababdbddfedcdd15c19dd77713b06cf5903d8ac
sparcbvba <info@sparc-design.be>
2020-08-03 16:18:42 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -13,6 +13,10 @@ class Tiles {
   _canvas: string;
   _map: number[][];
   _tileGraphics = [];
+  _settings = {
+    x: 100,
+    y: 100
+  };
 
   constructor(canvas: string) {
     this._canvas = canvas;
@@ -24,10 +28,10 @@ class Tiles {
     // Each number can represet a different graphic or possible enviroment interaction.
     // In this tutorial case we only have two possible tiles, zero which is blank and one which is filled.
     // Two Dimensional Array storing our isometric map layout. Each number represents a tile.
-    this._map = [[1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 1, 1], [1, 1, 1, 1]];
+    //this._map = [[1, 0, 0, 0], [1, 0, 0, 1], [0, 0, 1, 1], [1, 0, 1, 1]];
+    this._randomizeMap();
 
     // The next part grabs the canvas element id of 'main' within our page <body>.
-    
 
     // Using two for loops we run through each of the array rows stored and their element values.
     // for (var i = 0; i < this._map.length; i++) {
@@ -46,12 +50,25 @@ class Tiles {
     this._loadImg();
   }
 
+  private _randomizeMap() {
+    this._map = [];
+    for (var i = 0; i < this._settings.y; i++) {
+      this._map[i] = new Array(10);
+      for (var j = 0; j < this._settings.x; j++) {
+        this._map[i][j] = Math.random() > 0.1 ? 1 : 0;
+      }
+    }
+  }
+
   private _loadImg() {
-      
     // Images to be loaded and used.
     // Tutorial Note: As water is loaded first it will be represented by a 0 on the map and land will be a 1.
-    var tileGraphicsToLoad = ["./images/water.png","./images/land.png"],
-    tileGraphicsLoaded = 0;
+    var tileGraphicsToLoad = [
+        "https://jsiso.com/tutorials/images/water.png",
+        "https://jsiso.com/tutorials/images/land.png"
+      ],
+      tileGraphicsLoaded = 0;
+    const that = this;
 
     for (var i = 0; i < tileGraphicsToLoad.length; i++) {
       this._tileGraphics[i] = new Image();
@@ -61,15 +78,13 @@ class Tiles {
         // Once the image is loaded increment the loaded graphics count and check if all images are ready.
         tileGraphicsLoaded++;
         if (tileGraphicsLoaded === tileGraphicsToLoad.length) {
-            this._drawMap();
+          that._drawMap();
         }
-      }
-      console.log(tileGraphicsLoaded);
+      };
     }
   }
 
   private _drawMap() {
-      console.log('drawmap');
     // create the canvas context
     var canvasElement: any = document.getElementById(this._canvas);
     var ctx = canvasElement.getContext("2d");
@@ -77,10 +92,15 @@ class Tiles {
     // Set as your tile pixel sizes, alter if you are using larger tiles.
     var tileH = 25;
     var tileW = 52;
-    
+
     // mapX and mapY are offsets to make sure we can position the map as we want.
-    var mapX = 76;
-    var mapY = 52;
+    var mapX = this._settings.x * tileH;
+    var mapY = tileH;
+
+    canvasElement.width = mapX * 2.2;
+    canvasElement.height = (tileH * this._settings.y) + tileH * 2;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
     var drawTile;
 
@@ -89,7 +109,11 @@ class Tiles {
       for (var j = 0; j < this._map[i].length; j++) {
         drawTile = this._map[i][j];
         // Draw the represented image number, at the desired X & Y coordinates followed by the graphic width and height.
-        ctx.drawImage(this._tileGraphics[drawTile], (i - j) * tileH + mapX, (i + j) * tileH / 2 + mapY);
+        ctx.drawImage(
+          this._tileGraphics[drawTile],
+          (i - j) * tileH + mapX,
+          ((i + j) * tileH) / 2 + mapY
+        );
       }
     }
   }
